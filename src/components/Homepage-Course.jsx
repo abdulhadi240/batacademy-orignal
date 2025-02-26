d62e8f52d858257d21e4039b57f3862b35840222
abdulhadi240 <ah912425@gmail.com>
2025-02-27 02:18:14 +0500

    homepage

--- src/components/Homepage-Course.jsx
+++ src/components/Homepage-Course.jsx
@@ -62,7 +62,7 @@ export default function Home({ locale }) {
   };
 
   return (
-    <main className="min-h-screen bg-sky-50">
+    <main className="bg-sky-50">
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 items-center">
           {/* Centered Register Now section */}
